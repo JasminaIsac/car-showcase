249bdc6949c5103fb493287cc3bc033e61526c27
Jasmina <jasmina.isac@gmail.com>
2025-06-05 18:17:33 +0300

    Car images API

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -3,14 +3,14 @@ import { CarProps } from "@/types";
 import { Dialog, DialogPanel, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import Image from "next/image";
+import { generateCarImageUrl } from "@/utils";
+
 
 interface CardDetailsProps {
     isOpen: boolean;
     closeModal: () => void;
     car: CarProps;
 }
-
-
 
 const CardDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
     return (
@@ -46,18 +46,18 @@ const CardDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
                                     </button>
                                     <div className="flex flex-1 flex-col gap-3">
                                         <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
-                                            <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                            <Image src={generateCarImageUrl(car)} alt="car" fill priority className="object-contain" />
                                         </div>
                                         
                                         <div className="flex gap-3">
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "29")} alt="car" fill priority className="object-contain" />
                                             </div>
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "33")} alt="car" fill priority className="object-contain" />
                                             </div>
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "13")} alt="car" fill priority className="object-contain" />
                                             </div>
                                         </div>
                                     </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
         'X-RapidAPI-Key': '9798d72b04msh8bf6082c33da6fbp1564b6jsnc6fa69da43ec',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
         headers: headers,
     });
 
@@ -22,7 +24,7 @@ export function calculateRentalRate(city_mpg: any, year: number): number {
 
     const currentYear = new Date().getFullYear();
 
-    // Verificare dacă city_mpg este un număr valid, altfel se folosește 24
+    // If city_mpg is not a number or is NaN, use 24 as default
     const validCityMpg = typeof city_mpg === 'number' && !isNaN(city_mpg) ? city_mpg : 24;
 
     const mileageRate = validCityMpg * mileageFactor;
@@ -31,3 +33,74 @@ export function calculateRentalRate(city_mpg: any, year: number): number {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
     return Number(rentalRatePerDay.toFixed(2));
 }
+
+
+export const generateCarImageUrl = (car: any, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+  
+    const { make, year, model } = car;
+  
+    url.searchParams.append("customer", "hrjavascript-mastery");
+    url.searchParams.append("zoomType", "fullscreen");
+    url.searchParams.append("paintdescription", "radiant-green");
+    url.searchParams.append("modelFamily", model.split(" ")[0]);
+    url.searchParams.append("make", make);
+    url.searchParams.append("modelYear", `${year}`);
+    url.searchParams.append("angle", `${angle}`);
+  
+    return `${url}`;
+  };
+
+
+  //Vinaudit Demo API - not working
+
+// export const generateCarImageUrl = (car: CarProps) => {
+//     const modelSlug = car.model.toLowerCase().replace(/\s+/g, "_"); // ex: "corolla ce" => "corolla_ce"
+//     const carId = `${car.year}_${car.make.toLowerCase()}_${modelSlug}`;
+  
+//     const url = new URL("https://images.vinaudit.com/v3/images");
+//     url.searchParams.append("id", carId);
+//     url.searchParams.append("format", "json");
+//     url.searchParams.append("key", "VA_DEMO_KEY"); // cheia demo
+  
+//     return url.toString();
+//   };
+
+
+//Pexels API - working, but not needed images
+  
+// const API_KEY = "i1uDOCaf9B5OTKhhpnk4bCFpNxc7WFwfzdQDSCiLLRaMBetyJWoqYEA6";
+
+// export const fetchCarImageUrl = async (car: CarProps): Promise<string | null> => {
+//     try {
+//         const query = `${car.make} ${car.model} ${car.year} car`;
+//         const url = `https://api.pexels.com/v1/search?query=${encodeURIComponent(query)}&per_page=1&orientation=landscape`;
+        
+//         console.log('Fetching image for query:', query);
+        
+//         const response = await fetch(url, {
+//             headers: {
+//                 Authorization: API_KEY,
+//             },
+//         });
+        
+//         if (!response.ok) {
+//             console.error("Pexels API Error:", response.status, await response.text());
+//             return null;
+//         }
+        
+//         const data = await response.json();
+//         console.log('Pexels API response:', data);
+        
+//         if (data.photos && data.photos.length > 0) {
+//             // Use large2x instead of original for better performance
+//             return data.photos[0].src.large2x;
+//         } else {
+//             console.log('No photos found for:', query);
+//             return null;
+//         }
+//     } catch (error) {
+//         console.error('Error in fetchCarImageUrl:', error);
+//         return null;
+//     }
+// };
